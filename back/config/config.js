6436341092a9053824be0c427540fb6579811a8f
PabloMocholi <pablomocholi66@alumnos.cei.es>
2024-04-28 15:28:42 +0200

    usada toda la url para conectar

--- back/config/config.js
+++ back/config/config.js
@@ -11,3 +11,4 @@ export const fullDomain = `${domain}:${PORT}`
 export const DB_USER = process.env.DB_USER 
 export const DB_PASS = process.env.DB_PASS
 export const DB_DB = process.env.DB_DB 
+export const DB_MONGO = process.env.DB_MONGO
--- back/db/mongo.db.js
+++ back/db/mongo.db.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose'
-import { DB_DB, DB_PASS, DB_USER } from '../config/config.js'
+import { DB_DB, DB_MONGO, DB_PASS, DB_USER } from '../config/config.js'
 
 
 const connection = async () => {
     console.log("estableciendo conexion")
     const url = `mongodb+srv://${DB_USER}:${DB_PASS}@cei-pablo.3f5vxzt.mongodb.net/${DB_DB}`
+    
+    console.log(DB_MONGO)
     console.log(url)
+    //const url = DB_MONGO
 
-    await mongoose.connect(url)
+    await mongoose.connect(DB_MONGO)
         .then(console.log("conectado a MongoDB - ATLAS"))
         .catch(e => console.log("error en la conezxion ", e))
     
