cf15c66d9deca0aad8a7c81e835797cffe04ba27
PabloMocholi <pablomocholi66@alumnos.cei.es>
2024-04-28 15:37:24 +0200

    comentadas variables

--- back/config/config.js
+++ back/config/config.js
@@ -7,8 +7,8 @@ export const PORT = process.env.PORT || 3000
 export const domain = process.env.DOMAIN ||  "http://localhost"
 
 export const fullDomain = `${domain}:${PORT}`
-
+/*
 export const DB_USER = process.env.DB_USER 
 export const DB_PASS = process.env.DB_PASS
-export const DB_DB = process.env.DB_DB 
+export const DB_DB = process.env.DB_DB */
 export const DB_MONGO = process.env.DB_MONGO
